a89222a8e787347d6a9709508e4f31b47c3f154d
ox-i <oxi1224@o2.pl>
2022-02-14 13:45:36 +0100

    first

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let keyboard = document.getElementById('keyboard');
+let keys = [...keyboard.querySelectorAll('div.key')];
+
+document.addEventListener('keydown', (e) => {
+    e = e || window.event;
+    for (let num = 0; num < 13; ++num)
+    if(e.key == `F${num}` || e.key === "Tab" || e.key === "'" || e.key === "/" || e.key === "Alt"){
+        e.preventDefault();
+    }
+});
+function assignKey(event) {
+    let pressedKey = event.key;
+    for (let i = 0; i < keys.length; ++i) {
+        const key = keys[i].textContent;
+        if ((key).toLowerCase() === pressedKey.toLowerCase() || keys[i].id === event.code) {
+            keys[i].classList.add('toggledKey');
+            keys[i].classList.add('keyPressed');
+            let log = document.getElementById('log')
+            log.textContent = `<${key}> ${log.textContent}`
+        }
+    }
+} 
+function specialKeys(event) {
+    let pressedKey = event.key;
+    for (let i = 0; i < keys.length; ++i) {
+        const key = keys[i].textContent;
+        if ((key).toLowerCase() === pressedKey.toLowerCase() || keys[i].id === event.code) {
+            keys[i].classList.add('toggledKey');
+            keys[i].classList.remove('keyPressed');
+        }
+    }
+}
